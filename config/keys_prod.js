9a8ff6597d17c424f79f7a952519860e895baca8
Milos Mandic <mickozr@gmail.com>
2019-01-23 22:51:43 +0100

    apd

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -1,4 +1,4 @@
 module.exports = {
-  mongoURI: process.env.MONGO_URI,
-  secretOrKey: process.env.SECRET_OR_KEY
+  MongoURI: 'mongodb://mcmzrc:mcmzrc@cluster0-shard-00-00-mat0v.mongodb.net:27017,cluster0-shard-00-01-mat0v.mongodb.net:27017,cluster0-shard-00-02-mat0v.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true'
+  ,secretOrKey: 'secret'
 }
