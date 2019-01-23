7a3a28c05082ecd1c54a63947c96562374b9d06f
Milos Mandic <mickozr@gmail.com>
2019-01-23 21:55:27 +0100

    upd

--- server.js
+++ server.js
@@ -35,6 +35,6 @@ if(process.env.NODE_ENV === 'production'){
 }
 
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5001;
 
 app.listen(port , console.log(`server radi na portu ${port}`));
