c5ca727aa444474b93aee8e204331ca96ab81fe7
Milos Mandic <mickozr@gmail.com>
2019-01-23 21:47:23 +0100

    update app

--- server.js
+++ server.js
@@ -35,6 +35,6 @@ if(process.env.NODE_ENV === 'production'){
 }
 
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT , console.log(`server radi na portu ${PORT}`));
