355a51f94880dfa6fd3e79144c3b9539e5d1ac11
Milos Mandic <mickozr@gmail.com>
2019-01-23 20:58:57 +0100

    prep to deploy

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,6 @@
-module.exports = {
-  MongoURI: 'mongodb://mcmzrc:mcmzrc@cluster0-shard-00-00-mat0v.mongodb.net:27017,cluster0-shard-00-01-mat0v.mongodb.net:27017,cluster0-shard-00-02-mat0v.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true'
-  ,secretOrKey: 'secret'
+
+if(process.env.NODE_ENV === 'production'){
+  module.exports = require('./keys_prod');
+} else {
+  module.exports = require('./keys_dev');
 }
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const passport = require('passport');
+const path = require('path');
 
 const app = express();
 
@@ -24,6 +25,14 @@ require('./config/passport')(passport);
 app.use('/', require('./routes/index'));
 app.use('/users', require('./routes/users'));
 
+//server static assets if in production
+if(process.env.NODE_ENV === 'production'){
+	//set static folder
+	app.use(express.static('client/build'));
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+	});
+}
 
 
 const PORT = process.env.PORT || 5001;
